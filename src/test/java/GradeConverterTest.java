bbf4518af753aa5345fcd2eb8ba48d071330bafd
DuyTinNguyen182 <119142567+DuyTinNguyen182@users.noreply.github.com>
2025-06-11 15:49:08 +0700

    Update GradeConverterTest.java

--- src/test/java/GradeConverterTest.java
+++ src/test/java/GradeConverterTest.java
@@ -23,7 +23,7 @@ public class GradeConverterTest {
     @Test
     public void testConvert2() throws Exception {
         String expected = "A";
-        String actual = converter.convert(90);
+        String actual = converter.convert(80);
         assertEquals(expected,actual);
     }
     @Test
